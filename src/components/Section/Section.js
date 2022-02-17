b02b2341c962215fa4d0d257574b39b2ad40db3b
Mihailoolegovich <mihailoolegovich@gmail.com>
2022-02-17 22:04:23 +0200

    final version

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React from 'react';
+import './Section.css';
 
-const Section = ({title}) => (
-    <h2 className="title">{title}</h2>
-)
+const Section = ({ title }) => <h2 className="title">{title}</h2>;
 export default Section;
